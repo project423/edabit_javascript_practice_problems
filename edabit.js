ae3f5eafccef302381507b82b3905910a9d478d2
Andrew Clark <drewboy879@gmail.com>
2020-08-31 12:36:25 -0500

    first problem complete

---
+++ edabit.js
@@ -0,0 +1,9 @@
+// 1
+// Return Something to Me!
+// giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+function giveMeSomething(a){
+    return `something ${a}` 
+}
+
+console.log(giveMeSomething("a"))
