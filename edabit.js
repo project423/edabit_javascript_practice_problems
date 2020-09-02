36df0aecf985d525f15064b63d31f8378f152a87
Andrew Clark <drewboy879@gmail.com>
2020-09-01 22:53:42 -0500

    37 problems complete

--- edabit.js
+++ edabit.js
@@ -361,3 +361,230 @@
 // }
 
 // console.log(posCom(10))
+
+// 24
+// tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]) ➞ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// tuckIn([15,150], [45, 75, 35]) ➞ [15, 45, 75, 35, 150]
+
+// tuckIn([[1, 2], [5, 6]], [[3, 4]]) ➞ [[1, 2], [3, 4], [5, 6]]
+
+// function tuckIn(arr1, arr2) {
+//     return [arr1[0], ...arr2, arr1[1]]
+// }
+
+// console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9]))
+
+// 25
+// Go Corona!
+// endCorona(4000, 2000, 77000) ➞ 39
+
+// endCorona(3000, 2000, 50699) ➞ 51
+
+// endCorona(30000, 25000, 390205) ➞ 79
+
+// function endCorona(recovers, newCases, activeCases) {
+//     days = 0
+//     while (activeCases > 0) {
+//         activeCases += newCases
+//         activeCases -= recovers
+//         days +=1
+//     }
+//     return days
+// }
+
+// console.log(endCorona(4000, 2000, 77000))
+
+// 26
+
+// Make My Way Home
+
+// distanceHome([2, 4, 2, 5]) ➞ 13
+
+// distanceHome([-1, -4, -3, -2]) ➞ 10
+
+// distanceHome([3, 4, -5, -2]) ➞ 0
+
+
+// function distanceHome(arr) {
+// 	return Math.abs(arr.reduce((accumulator, currentValue) => accumulator + currentValue))
+// }
+
+// console.log(distanceHome([-1, -4, -3, -2]))
+
+// 27
+// Get the File Name
+
+// getFilename("C:/Projects/pil_tests/ascii/edabit.txt") ➞ "edabit.txt"
+
+// getFilename("C:/Users/johnsmith/Music/Beethoven_5.mp3") ➞ "Beethoven_5.mp3"
+
+// getFilename("ffprobe.exe") ➞ "ffprobe.exe"
+
+// function getFilename(path) {
+// 	return path.split('/').pop()
+// }
+
+// console.log(getFilename("C:/Projects/pil_tests/ascii/edabit.txt"))
+
+//28
+
+// Modifying the Last Character
+
+// modifyLast("Hello", 3) ➞ "Hellooo"
+
+// modifyLast("hey", 6) ➞ "heyyyyyy"
+
+// modifyLast("excuse me what?", 5) ➞ "excuse me what?????"
+
+// function modifyLast(str, n) {
+// 	return str + str.charAt(str.length-1).repeat(n-1)
+// return str + str.slice(-1).repeat(n - 1)
+// }
+
+// console.log(modifyLast("hey", 6))
+
+
+//29
+// Halloween Day
+
+// halloween(new Date("2013/10/31")) ➞ "Bonfire toffee"
+
+// halloween(new Date("2012/07/31")) ➞ "toffee"
+
+// halloween(new Date("2011/10/12")) ➞ "toffee"
+
+// function halloween(dt) {
+//     if (dt.getMonth() == 9 && dt.getDate() == 31){
+//         return 'Bonfire toffee'
+//     }
+//     return 'toffee'
+// }
+
+// console.log(halloween(new Date("2013/10/31")))
+
+//30
+
+// getFilename("C:/Projects/pil_tests/ascii/edabit.txt") ➞ "edabit.txt"
+
+// getFilename("C:/Users/johnsmith/Music/Beethoven_5.mp3") ➞ "Beethoven_5.mp3"
+
+// getFilename("ffprobe.exe") ➞ "ffprobe.exe"
+
+// function getFilename(path) {
+// 	return path.split('/').pop()
+// }
+
+// console.log("C:/Projects/pil_tests/ascii/edabit.txt")
+
+// 31
+// Half, Quarter and Eighth
+// Create a function that takes a number and return an array of three numbers: half of the
+// number, quarter of the number and an eighth of the number.
+
+// halfQuarterEighth(6) ➞ [3, 1.5, 0.75]
+
+// halfQuarterEighth(22) ➞ [11, 5.5, 2.75]
+
+// halfQuarterEighth(25) ➞ [12.5, 6.25, 3.125]
+
+// function halfQuarterEighth(n) {
+// 	return [n/2, n/4, n/8]
+// }
+
+// console.log(halfQuarterEighth(22))
+
+//32
+// firstLast("ganesh") ➞ "gh"
+
+// firstLast("kali") ➞ "ki"
+
+// firstLast("shiva") ➞ "sa"
+
+// firstLast("vishnu") ➞ "vu"
+
+// firstLast("durga") ➞ "da"
+
+// function firstLast(name) {
+// 	return name.slice(0,1) + name.slice(-1)
+// }
+
+// console.log(firstLast("vishnu"))
+
+//33
+// Burrrrrrrp
+
+// longBurp(3) ➞ "Burrrp"
+
+// longBurp(5) ➞ "Burrrrrp"
+
+// longBurp(9) ➞ "Burrrrrrrrrp"
+
+// function longBurp(num) {
+// 	return `Bu${'r'.repeat(num)}p`
+// }
+
+// console.log(longBurp(3))
+
+//34
+
+// cubeSquareRoot(81) ➞ 729
+
+// cubeSquareRoot(1646089) ➞ 2111932187
+
+// cubeSquareRoot(695556) ➞ 580093704
+
+// function cubeSquareRoot(num) {
+// 	return (num ** (1/2)) ** 3
+// }
+
+// console.log(cubeSquareRoot(81))
+
+//35
+// Squares and Cubes
+// Create a function that takes an array of two numbers and checks if the square root of the
+// first number is equal to the cube root of the second number.
+
+// checkSquareAndCube([4, 8]) ➞ true
+
+// checkSquareAndCube([16, 48]) ➞ false
+
+// checkSquareAndCube([9, 27]) ➞ true
+
+// function checkSquareAndCube(arr) {
+// 	return (arr[0] ** (1/2)) == (Math.cbrt(arr[1]))
+// }
+
+// const checkSquareAndCube = ns => Math.sqrt(ns[0]) ** 3 === ns[1];
+// console.log(checkSquareAndCube([25, 125]))
+
+//36
+// True Ones, False Zeros
+
+// integerBoolean("100101") ➞ [true, false, false, true, false, true]
+
+// integerBoolean("10") ➞ [true, false]
+
+// integerBoolean("001") ➞ [false, false, true]
+
+
+// function integerBoolean(n) {
+// 	return n.split('').map(x => Boolean(parseInt(x)))
+// }
+// const integerBoolean = n => [...n].map(a => a == 1);
+
+// console.log(integerBoolean("100101"))
+
+//37
+// Broken Bridge
+
+// isSafeBridge("####") ➞ true
+
+// isSafeBridge("## ####") ➞ false
+
+// isSafeBridge("#") ➞ true
+
+// function isSafeBridge(str) {
+// 	return !str.includes(' ')
+// }
+
